c8f82abc1725effdcb567a3c2b7e30b9eabc2f7b
jimothy911 <150150156+jimothy911@users.noreply.github.com>
2023-11-23 09:05:20 -0500

    Background for the Seal since he needs the sea
    reprsents the habitat for the seal

--- MyWorld.java
+++ MyWorld.java
@@ -18,6 +18,8 @@ public class MyWorld extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
         Seal seeeeel = new Seal();
+        Sea seea = new Sea();
+        addObject(seea, 50, 100);
         addObject(seeeeel, 69, 69);
     }
 }
